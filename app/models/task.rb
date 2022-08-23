c240f6739137c1cb588d54f46e321653364068ac
Ahmed.O <ao.osman14@outlook.com>
2022-08-23 19:23:24 +0100

    rails new

---
+++ app/models/task.rb
@@ -0,0 +1,12 @@
+class Task < ApplicationRecord
+  def change
+    create_table  : tasks do |t|
+      t.string    : title
+      t.text      : description
+      t.boolean   : completed, default: false
+
+      t.timestamps
+    end
+  end
+ end
+end
